3b65cd2a449463ea9924dab40dfd5665b79832f3
Chris Cowan <agentme49@gmail.com>
2022-12-07 22:24:53 -0800

    solve day 8 part 2

--- day_8.ts
+++ day_8.ts
@@ -77,14 +77,65 @@ function part1(input: string): number {
   return visibleTrees;
 }
 
-// function part2(input: string): number {
-//   const grid = parse(input);
-//   throw new Error("TODO");
-// }
+function calculateScenicScoreOfTree(
+  grid: number[][],
+  x: number,
+  y: number,
+): number {
+  const treeHeight = grid[y][x];
+
+  const width = grid[0].length;
+  const height = grid.length;
+
+  let leftVisibleTrees = 0;
+  for (let x2 = x - 1; x2 >= 0; x2--) {
+    leftVisibleTrees++;
+    if (grid[y][x2] >= treeHeight) {
+      break;
+    }
+  }
+  let rightVisibleTrees = 0;
+  for (let x2 = x + 1; x2 < width; x2++) {
+    rightVisibleTrees++;
+    if (grid[y][x2] >= treeHeight) {
+      break;
+    }
+  }
+  let upVisibleTrees = 0;
+  for (let y2 = y - 1; y2 >= 0; y2--) {
+    upVisibleTrees++;
+    if (grid[y2][x] >= treeHeight) {
+      break;
+    }
+  }
+  let downVisibleTrees = 0;
+  for (let y2 = y + 1; y2 < height; y2++) {
+    downVisibleTrees++;
+    if (grid[y2][x] >= treeHeight) {
+      break;
+    }
+  }
+  return leftVisibleTrees * rightVisibleTrees *
+    upVisibleTrees * downVisibleTrees;
+}
+
+function part2(input: string): number {
+  const grid = parse(input);
+  let maxScenicScore = 0;
+  for (let y = 0; y < grid.length; y++) {
+    for (let x = 0; x < grid[0].length; x++) {
+      const scenicScore = calculateScenicScoreOfTree(grid, x, y);
+      if (scenicScore > maxScenicScore) {
+        maxScenicScore = scenicScore;
+      }
+    }
+  }
+  return maxScenicScore;
+}
 
 if (import.meta.main) {
   runPart(2022, 8, 1, part1);
-  // runPart(2022, 8, 2, part2);
+  runPart(2022, 8, 2, part2);
 }
 
 const TEST_INPUT = `\
@@ -99,6 +150,6 @@ Deno.test("part1", () => {
   assertEquals(part1(TEST_INPUT), 21);
 });
 
-// Deno.test("part2", () => {
-//   assertEquals(part2(TEST_INPUT), 12);
-// });
+Deno.test("part2", () => {
+  assertEquals(part2(TEST_INPUT), 8);
+});
