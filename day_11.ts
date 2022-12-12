d5995aa3993f32560a1600cbdfc84127d8f0b788
Chris Cowan <agentme49@gmail.com>
2022-12-12 06:32:10 -0800

    solve day 11 part 1

---
+++ day_11.ts
@@ -0,0 +1,161 @@
+import { chunk } from "https://deno.land/std@0.167.0/collections/chunk.ts";
+import { sortBy } from "https://deno.land/std@0.167.0/collections/sort_by.ts";
+import { assertEquals } from "https://deno.land/std@0.167.0/testing/asserts.ts";
+import { runPart } from "https://deno.land/x/aocd@v1.3.0/mod.ts";
+
+interface Operation {
+  label: string;
+  fn(old: number): number;
+}
+
+type Operator = (a: number, b: number) => number;
+
+const operators: { [symbol: string]: Operator } = {
+  "+": (a: number, b: number) => a + b,
+  "-": (a: number, b: number) => a - b,
+  "*": (a: number, b: number) => a * b,
+};
+
+function parseOperationFromString(operation: string): Operation {
+  const m = /^new\s*=\s*(.*)$/.exec(operation);
+  if (!m) {
+    throw new Error(`Invalid operation: ${operation}`);
+  }
+  const [, expression] = m;
+  const [leftStr, operatorStr, rightStr] = expression.split(/\s+/);
+  const left: number | null = leftStr === "old" ? null : Number(leftStr);
+  const right: number | null = rightStr === "old" ? null : Number(rightStr);
+  if (!Object.hasOwn(operators, operatorStr)) {
+    throw new Error(`Ivalid operator: ${operatorStr}`);
+  }
+  const operator = operators[operatorStr];
+  return {
+    label: operation,
+    fn: (old: number) => {
+      const leftValue = left === null ? old : left;
+      const rightValue = right === null ? old : right;
+      return operator(leftValue, rightValue);
+    },
+  };
+}
+
+interface Test {
+  label: string;
+  fn(value: number): boolean;
+}
+
+function parseTest(test: string): Test {
+  const m = /^divisible by (\d+)$/.exec(test);
+  if (!m) {
+    throw new Error(`Invalid test: ${test}`);
+  }
+  const [, divisorStr] = m;
+  const divisor = Number(divisorStr);
+  return {
+    label: test,
+    fn: (value: number) => value % divisor === 0,
+  };
+}
+
+interface Monkey {
+  items: number[];
+  operation: Operation;
+  test: Test;
+  destIfTrue: number;
+  destIfFalse: number;
+}
+
+function parse(input: string): Monkey[] {
+  const lines = input.trimEnd().split("\n");
+  const chunks = chunk(lines, 7);
+  return chunks.map((lines, index) => {
+    assertEquals(lines[0], `Monkey ${index}:`);
+    return {
+      items: lines[1].split(": ")[1].split(", ").map(Number),
+      operation: parseOperationFromString(lines[2].split(": ")[1]),
+      test: parseTest(lines[3].split(": ")[1]),
+      destIfTrue: Number(
+        /^\s*If true: throw to monkey (\d+)$/.exec(lines[4])![1],
+      ),
+      destIfFalse: Number(
+        /^\s*If false: throw to monkey (\d+)$/.exec(lines[5])![1],
+      ),
+    };
+  });
+}
+
+function part1(input: string): number {
+  const monkeys = parse(input);
+  const monkeyInspectionCounters = new Map(
+    monkeys.map((monkey) => [monkey, 0]),
+  );
+  for (let round = 0; round < 20; round++) {
+    for (const monkey of monkeys) {
+      for (const worryLevel of monkey.items) {
+        const newWorryLevel = Math.floor(monkey.operation.fn(worryLevel) / 3);
+        const dest = monkey.test.fn(newWorryLevel)
+          ? monkey.destIfTrue
+          : monkey.destIfFalse;
+        monkeys[dest].items.push(newWorryLevel);
+        monkeyInspectionCounters.set(
+          monkey,
+          monkeyInspectionCounters.get(monkey)! + 1,
+        );
+      }
+      monkey.items.length = 0;
+    }
+  }
+  const sorted = sortBy(
+    Array.from(monkeyInspectionCounters.entries()),
+    ([, count]) => -count,
+  );
+  return sorted[0][1] * sorted[1][1];
+}
+
+// function part2(input: string): number {
+//   const monkeys = parse(input);
+//   throw new Error("TODO");
+// }
+
+if (import.meta.main) {
+  runPart(2022, 11, 1, part1);
+  // runPart(2022, 11, 2, part2);
+}
+
+const TEST_INPUT = `\
+Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+Monkey 1:
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+Monkey 2:
+  Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
+
+Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1
+`;
+
+Deno.test("part1", () => {
+  assertEquals(part1(TEST_INPUT), 10605);
+});
+
+// Deno.test("part2", () => {
+//   assertEquals(part2(TEST_INPUT), 12);
+// });
