55a77d2bcb6e0a75a851e7a92f3521cfcb61d5d7
Chris Cowan <agentme49@gmail.com>
2022-12-12 05:30:56 -0800

    solve day 10 part 1

---
+++ cpu.ts
@@ -0,0 +1,71 @@
+export type Instruction = { type: "noop" } | { type: "addx"; value: number };
+
+export function parseInstructions(input: string): Instruction[] {
+  return input.trimEnd().split("\n").map((line) => {
+    const [type, value] = line.split(" ");
+    if (type === "noop") {
+      return { type: "noop" };
+    }
+    return { type: "addx", value: Number(value) };
+  });
+}
+
+export class Machine {
+  public x = 1;
+  public pc = 0;
+  /**
+   * Represents the cycle number that the machine is starting.
+   * The machine has successfully executed `cycle - 1` cycles already.
+   */
+  public cycle = 1;
+  #currentInstructionBeganOnCycle = 0;
+  #complete = false;
+  #instructions: Instruction[];
+
+  constructor(instructions: Instruction[]) {
+    this.#instructions = instructions;
+  }
+
+  run(cycles: number) {
+    if (this.#complete) {
+      return;
+    }
+    for (let i = 0; i < cycles; i++) {
+      const instruction = this.#instructions[this.pc];
+      if (!instruction) {
+        this.#complete = true;
+        break;
+      }
+      switch (instruction.type) {
+        case "noop": {
+          this.pc++;
+          this.#currentInstructionBeganOnCycle = this.cycle + 1;
+          break;
+        }
+        case "addx": {
+          if (this.#currentInstructionBeganOnCycle + 1 === this.cycle) {
+            this.x += instruction.value;
+            this.pc++;
+            this.#currentInstructionBeganOnCycle = this.cycle + 1;
+          }
+          break;
+        }
+        default: {
+          throw new Error(
+            // deno-lint-ignore no-explicit-any
+            `Unknown instruction type: ${(instruction as any).type}`,
+          );
+        }
+      }
+      this.cycle++;
+      if (this.pc >= this.#instructions.length) {
+        this.#complete = true;
+        break;
+      }
+    }
+  }
+
+  get complete(): boolean {
+    return this.#complete;
+  }
+}
---
+++ day_10.ts
@@ -0,0 +1,214 @@
+import { assertEquals } from "https://deno.land/std@0.167.0/testing/asserts.ts";
+import { runPart } from "https://deno.land/x/aocd@v1.3.0/mod.ts";
+import { Machine, parseInstructions } from "./cpu.ts";
+
+function part1(input: string): number {
+  const instructions = parseInstructions(input);
+  const machine = new Machine(instructions);
+  let sum = 0;
+  for (let desiredCycle = 20;; desiredCycle += 40) {
+    machine.run(desiredCycle - machine.cycle);
+    if (machine.complete) {
+      break;
+    }
+    sum += machine.cycle * machine.x;
+  }
+  return sum;
+}
+
+// function part2(input: string): number {
+//   const items = parse(input);
+//   throw new Error("TODO");
+// }
+
+if (import.meta.main) {
+  runPart(2022, 10, 1, part1);
+  // runPart(2022, 10, 2, part2);
+}
+
+const SIMPLE_TEST_INPUT = `\
+noop
+addx 3
+addx -5
+`;
+
+Deno.test("simple", () => {
+  const machine = new Machine(parseInstructions(SIMPLE_TEST_INPUT));
+  assertEquals(machine.cycle, 1);
+  assertEquals(machine.x, 1);
+  machine.run(1);
+  assertEquals(machine.cycle, 2);
+  assertEquals(machine.x, 1);
+  machine.run(1);
+  assertEquals(machine.cycle, 3);
+  assertEquals(machine.x, 1);
+  machine.run(1);
+  assertEquals(machine.cycle, 4);
+  assertEquals(machine.x, 4);
+  machine.run(1);
+  assertEquals(machine.cycle, 5);
+  assertEquals(machine.x, 4);
+  assertEquals(machine.complete, false);
+  machine.run(1);
+  assertEquals(machine.cycle, 6);
+  assertEquals(machine.x, -1);
+  assertEquals(machine.complete, true);
+});
+
+const TEST_INPUT = `\
+addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
+`;
+
+Deno.test("part1", () => {
+  // The challenge page says the sample answer is 13140, but it's wrong.
+  assertEquals(part1(TEST_INPUT), 13360);
+});
+
+// Deno.test("part2", () => {
+//   assertEquals(part2(TEST_INPUT), 12);
+// });
